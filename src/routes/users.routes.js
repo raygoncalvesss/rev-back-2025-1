9a1a357ca3fb6ab9b6d18e63459e74e954c6f71b
rayssa <rayssagoncalves.tds2.senai@gmail.com>
2025-02-06 08:37:40 -0300

    feat:create routes and routes control

---
+++ src/routes/users.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+import {
+    getUsers,
+    getUsersById,
+    createUser,
+    updateUser,
+    deleteUser,
+} from "../controllers/users.controllers.js"
+
+const usersRouter = Router();
+
+usersRouter.get("/", getUsers);
+usersRouter.get("/:id", getUsersById);
+usersRouter.post("/", createUser);
+usersRouter.put("/:id", updateUser);
+usersRouter.delete("/:id", deleteUser);
+
+export default usersRouter;
+
